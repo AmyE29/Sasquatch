9365dcb9688080a7caaec29f990a7e6b2b26e9c5
Amy Evans <amykarline@yahoo.com>
2019-10-21 15:15:27 -0700

    added all the card prompts and answer options

--- cardOptions.js
+++ cardOptions.js
@@ -9,18 +9,32 @@
 
 //(here is where all the words will be typed out for every card prompt statement, and every card's options)
 //example:
-var berriesCardPrompt = 'You come across some berries.  Do you?';
-var berriesOption1 = 'Eat them';
-var berriesOption2 =  'Throw them at Big Foot to slow him down';
-var berriesOption3 =  'Avoid them, they might be poisonous';
-var berriesOption4 = 'Collect them to make a pie later';
+var berriesCardPrompt = 'You come across some berries.  Do you...';
+var berriesOption1 = 'Eat them? They might give you more energy';
+var berriesOption2 =  'Throw them at Big Foot to slow him down?';
+var berriesOption3 =  'Avoid them, they might be poisonous?';
+var berriesOption4 = 'Collect them to make a pie later?';
 
-var snakesCardPrompt = 'Oh no! Snakes on the trail.  Do you?';
+var snakesCardPrompt = 'Oh no! Snakes on the trail.  Do you...';
+var snakesOption1 =  'Run screaming in the other direction?';
+var snakesOption2 =  'Go all Samuel L Jackson, and \'Get the #$@&%*! Snakes, off the #$@&%*! Trail!!\'" ?';
+var snakesOption3 =  'Eat them? They might give you more energy';
+var snakesOption4 =  'Train them to attck Big Foot?';
 
+var treeCardPrompt = 'You are blocked by a fallen tree.  Do you...';
+var treeOption1 = 'Leap over it?';
+var treeOption2 =  'Try to hide behind it?';
+var treeOption3 =  'Go around it?';
+var treeOption4 = 'Dig under it?';
 
-var treeCardPrompt = 'You are blocked by a fallen tree.  Do you?';
+var streamCardPrompt = 'Ack! There\'s a stream.  Do you...';
+var streamOption1 = 'Swim across it?';
+var streamOption2 =  'Stop for a quick drink?';
+var streamOption3 =  'Catapult over it?';
+var streamOption4 = 'Try to hide under water and hope Big Foot doesn\'t see you?';
 
-
-// eslint-disable-next-line no-unused-vars
-var streamCardPrompt = 'You have come to a stream.  Do you?';
-
+var tripCardPrompt = 'Ooops! You trip over some roots, and hurt your ankle.  Do you...';
+var tripOption1 = 'Pick yourself up and keep running?';
+var tripOption2 =  'Rest and bit, and massage it?';
+var tripOption3 =  'Rub some dirt on it?';
+var tripOption4 = 'Lay down and cry?';
